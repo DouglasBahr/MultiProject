03ea16e73b40a12f9beddca87e0b1ab449e46848
Douglas Bahr <douglasbahr1@gmail.com>
2021-02-10 18:16:06 -0800

    Creating a template file to help format code.

---
+++ template.py
@@ -0,0 +1,19 @@
+"""
+Title: Template
+
+Date: 4/1/2020
+
+Author: Sinthirll
+
+Description: This is a description for how a file might
+             be best formated in terms of standard practices. 
+ 
+"""
+
+#Python import#e.g. from time import sleep
+
+#Custom imports  #e.g. from file import class as name
+
+#Constants  # e.g. GENERIC_CONSTANT = "Status_Test_String"
+
+#Code
